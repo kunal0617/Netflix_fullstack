ce5a94defa247ad86200410d7e53e789e5ec9fe5
kunal singh <anandkunal0617@gmail.com>
2021-11-27 11:25:19 +0530

    Update firebase.js

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBnAxKXJdzmoNFKhNx_AGhPkwVlE7J7780",
-  authDomain: "netflix-ae1fb.firebaseapp.com",
-  projectId: "netflix-ae1fb",
-  storageBucket: "netflix-ae1fb.appspot.com",
-  messagingSenderId: "383022165930",
-  appId: "1:383022165930:web:2125675140c3044e6105d3",
-  measurementId: "G-F2LTDK6DE1",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
